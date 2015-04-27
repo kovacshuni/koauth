3106cf65bfdb6013105d105f9a1ebd2b39353307
Hunor Kovács <hunor.kovacs@ft.com>
2015-04-27 16:46:03 +0300

    Removed Futures from mappers.

--- src/main/scala/com/hunorkovacs/koauthsync/domain/mapper/RequestMapper.scala
+++ src/main/scala/com/hunorkovacs/koauthsync/domain/mapper/RequestMapper.scala
@@ -1,9 +1,8 @@
 package com.hunorkovacs.koauthsync.domain.mapper
 
 import com.hunorkovacs.koauthsync.domain.KoauthRequest
-import scala.concurrent.{ExecutionContext, Future}
 
 trait RequestMapper[RequestType] {
 
-  def map(source: RequestType): Future[KoauthRequest]
+  def map(source: RequestType): KoauthRequest
 }
--- src/main/scala/com/hunorkovacs/koauthsync/domain/mapper/ResponseMapper.scala
+++ src/main/scala/com/hunorkovacs/koauthsync/domain/mapper/ResponseMapper.scala
@@ -1,9 +1,8 @@
 package com.hunorkovacs.koauthsync.domain.mapper
 
-import scala.concurrent.Future
 import com.hunorkovacs.koauthsync.domain.KoauthResponse
 
 trait ResponseMapper[ResponseType] {
 
-  def map(source: KoauthResponse): Future[ResponseType]
+  def map(source: KoauthResponse): ResponseType
 }
