7fb9f0b93cc32737ad89f85d57cba2b6ce5423ac
Hunor Kovacs <kovacshuni@yahoo.com>
2017-03-29 16:39:12 +0300

    Moved out sync ConsumerService to be the only and main. No futures used in ConsumerService as it doesn't really have io or long lasting computations, so no reason.

--- src/main/scala/com/hunorkovacs/koauth/service/consumer/ConsumerService.scala
+++ src/main/scala/com/hunorkovacs/koauth/service/consumer/ConsumerService.scala
@@ -1,75 +1,102 @@
 package com.hunorkovacs.koauth.service.consumer
 
 import com.hunorkovacs.koauth.domain.KoauthRequest
+import com.hunorkovacs.koauth.domain.OauthParams._
+import com.hunorkovacs.koauth.service.Arithmetics.{concatItemsForSignature, createAuthorizationHeader, sign}
+import com.hunorkovacs.koauth.service.DefaultTokenGenerator.generateNonce
 
-import scala.concurrent.{ExecutionContext, Future}
+case class RequestWithInfo(request: KoauthRequest, signatureBase: String, header: String)
 
 trait ConsumerService {
 
   def createRequestTokenRequest(request: KoauthRequest,
                                 consumerKey: String,
                                 consumerSecret: String,
-                                callback: String): Future[RequestWithInfo]
+                                callback: String): RequestWithInfo
 
   def createAccessTokenRequest(request: KoauthRequest,
                                consumerKey: String,
                                consumerSecret: String,
                                requestToken: String,
                                requestTokenSecret: String,
-                               verifier: String): Future[RequestWithInfo]
+                               verifier: String): RequestWithInfo
 
   def createOauthenticatedRequest(request: KoauthRequest,
                                   consumerKey: String,
                                   consumerSecret: String,
                                   requestToken: String,
-                                  requestTokenSecret: String): Future[RequestWithInfo]
+                                  requestTokenSecret: String): RequestWithInfo
 
-  def createGeneralSignedRequest(request: KoauthRequest): Future[RequestWithInfo]
+  def createGeneralSignedRequest(request: KoauthRequest): RequestWithInfo
 }
 
-case class RequestWithInfo(request: KoauthRequest, signatureBase: String, header: String)
+object DefaultConsumerService extends ConsumerService {
 
+  private val secretNames = Set(ConsumerSecretName, TokenSecretName)
 
-class DefaultConsumerService(private val ec: ExecutionContext) extends ConsumerService {
-
-  implicit private val implicitEc = ec
-
-  override def createRequestTokenRequest(request: KoauthRequest,
-                                         consumerKey: String,
-                                         consumerSecret: String,
-                                         callback: String): Future[RequestWithInfo] = {
-    Future {
-      synch.ConsumerService.createRequestTokenRequest(request, consumerKey, consumerSecret, callback)
-    }
+  def createRequestTokenRequest(request: KoauthRequest,
+                                consumerKey: String,
+                                consumerSecret: String,
+                                callback: String): RequestWithInfo = {
+    createGeneralSignedRequest(
+      KoauthRequest(request, ConsumerKeyName -> consumerKey
+        :: ConsumerSecretName -> consumerSecret
+        :: CallbackName -> callback
+        :: basicParamList())
+    )
   }
 
-  override def createAccessTokenRequest(request: KoauthRequest,
-                                        consumerKey: String,
-                                        consumerSecret: String,
-                                        requestToken: String,
-                                        requestTokenSecret: String,
-                                        verifier: String): Future[RequestWithInfo] = {
-    Future {
-      synch.ConsumerService.createAccessTokenRequest(request, consumerKey, consumerSecret, requestToken, requestTokenSecret, verifier)
-    }
+  def createAccessTokenRequest(request: KoauthRequest,
+                               consumerKey: String,
+                               consumerSecret: String,
+                               requestToken: String,
+                               requestTokenSecret: String,
+                               verifier: String): RequestWithInfo = {
+    createGeneralSignedRequest(
+      KoauthRequest(request, ConsumerKeyName -> consumerKey
+        :: ConsumerSecretName -> consumerSecret
+        :: TokenName -> requestToken
+        :: TokenSecretName -> requestTokenSecret
+        :: VerifierName -> verifier
+        :: basicParamList()
+      )
+    )
   }
 
-  override def createOauthenticatedRequest(request: KoauthRequest,
-                                           consumerKey: String,
-                                           consumerSecret: String,
-                                           requestToken: String,
-                                           requestTokenSecret: String): Future[RequestWithInfo] = {
-    Future {
-      synch.ConsumerService.createOauthenticatedRequest(request, consumerKey, consumerSecret, requestToken, requestTokenSecret)
-    }
+  def createOauthenticatedRequest(request: KoauthRequest,
+                                  consumerKey: String,
+                                  consumerSecret: String,
+                                  requestToken: String,
+                                  requestTokenSecret: String): RequestWithInfo = {
+    createGeneralSignedRequest(
+      KoauthRequest(request, ConsumerKeyName -> consumerKey
+        :: ConsumerSecretName -> consumerSecret
+        :: TokenName -> requestToken
+        :: TokenSecretName -> requestTokenSecret
+        :: basicParamList())
+    )
   }
 
-  def createGeneralSignedRequest(request: KoauthRequest): Future[RequestWithInfo] = {
-    Future {
-      synch.ConsumerService.createGeneralSignedRequest(request)
-    }
+  def createGeneralSignedRequest(request: KoauthRequest): RequestWithInfo = {
+    val consumerSecret = request.oauthParamsMap.applyOrElse(ConsumerSecretName, (s: String) => "")
+    val tokenSecret = request.oauthParamsMap.applyOrElse(TokenSecretName, (s: String) => "")
+    val base = createSignatureBase(request)
+    val header = createAuthorizationHeader(SignatureName -> sign(base, consumerSecret, tokenSecret)
+      :: request.oauthParamsList.filterNot(p => secretNames(p._1)))
+    RequestWithInfo(request, base, header)
   }
+
+  def createSignatureBase(request: KoauthRequest): String = concatItemsForSignature(KoauthRequest(
+    request.method,
+    request.urlWithoutParams,
+    request.urlParams,
+    request.bodyParams,
+    request.oauthParamsList.filterNot(p => secretNames(p._1))))
+
+  private def basicParamList() = List(
+    NonceName -> generateNonce,
+    VersionName -> "1.0",
+    SignatureMethodName -> "HMAC-SHA1",
+    TimestampName -> (System.currentTimeMillis() / 1000).toString
+  )
 }
-
-
-
--- src/test/scala/com/hunorkovacs/koauth/service/consumer/ConsumerServiceSpec.scala
+++ src/test/scala/com/hunorkovacs/koauth/service/consumer/ConsumerServiceSpec.scala
@@ -37,7 +37,7 @@ class ConsumerServiceSpec extends Specification {
     ", oauth_version=\"1.0\""
   val SignatureBase = "POST&https%3A%2F%2Fapi.twitter.com%2F1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue%26oauth_consumer_key%3Dxvz1evFS4wEEPTGEFPHBog%26oauth_nonce%3DkYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1318622958%26oauth_token%3D370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb%26oauth_version%3D1.0%26status%3DHello%2520Ladies%2520%252B%2520Gentlemen%252C%2520a%2520signed%2520OAuth%2520request%2521"
 
-  import ConsumerService._
+  import com.hunorkovacs.koauth.service.consumer.DefaultConsumerService._
 
   "Creating a 'Request Token' request" should {
     "include all the necessary OAuth parameters." in {
